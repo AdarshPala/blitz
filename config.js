908c1f90ab98eecf330b17c3d73599e28c180fea
AdarshPala <adarsh_pala@hotmail.com>
2020-11-30 17:07:56 -0500

    Add and use a config file

---
+++ config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  NUMBER_OF_REQUESTS: 42,
+  TIME_IN_MS_BETWEEN_EACH_REQUEST: 1000,
+  PORT: 3002,
+  GRAPH_WIDTH: 400,
+  GRAPH_HEIGHT: 400,
+  ENDPOINT: 'file',
+  GRAPH_FILE_NAME: 'testimage.png',
+};
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const request = require('superagent');
 const responseTime = require('superagent-response-time');
 const { CanvasRenderService } = require('chartjs-node-canvas');
+const config = require('./config');
 
 const xAxisLabels = [];
 const latencies = [];
@@ -15,33 +16,30 @@ const callback = (req, time) => {
 const agent = request.agent().use(responseTime(callback));
 console.log('Sending requests to the server...');
 
-//let x = 0;
 const perfTest = new Promise((resolve, reject) => {
-  for (let i = 0; i < 92; i++) {
-    //x++;
-    //setTimeout(() => {
-    xAxisLabels.push(i);
-    agent
-      //.get(`http://localhost:3001/fib/${i}`)
-      .get('http://localhost:3002/file')
-      .then((res) => {
-        // res.body, res.headers, res.status
-        //console.log(res.body);
-        if (latencies.length === 92) {
-          resolve();
-        }
-      })
-      .catch((err) => {
-        // err.message, err.response
-        console.log('Error: ', err.message);
-      });
-  //}, 3000 * x);
+  for (let i = 0; i < config.NUMBER_OF_REQUESTS; i++) {
+    setTimeout(() => {
+      xAxisLabels.push(i);
+      agent
+        //.get(`http://localhost:${config.PORT}/${config.ENDPOINT}/${i}`)
+        .get(`http://localhost:${config.PORT}/${config.ENDPOINT}`)
+        .then((res) => {
+          console.log(res.body);
+          if (latencies.length === config.NUMBER_OF_REQUESTS) {
+            resolve();
+          }
+        })
+        .catch((err) => {
+          console.log('Error: ', err.message);
+          reject();
+        });
+    }, config.TIME_IN_MS_BETWEEN_EACH_REQUEST * i);
   }
-})
-const width = 400;
-const height = 400;
+});
+
+const width = config.GRAPH_WIDTH;
+const height = config.GRAPH_HEIGHT;
 const chartCallback = (ChartJS) => {
- 
     // Global config example: https://www.chartjs.org/docs/latest/configuration/
     ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
     // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
@@ -56,49 +54,55 @@ const chartCallback = (ChartJS) => {
 const canvasRenderService = new CanvasRenderService(width, height, chartCallback);
  
 (async () => {
-  await perfTest;
-    const configuration = {
-        type: 'line',
-        data: {
-            //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            labels: xAxisLabels,
-            datasets: [{
-                label: 'Latency',
-                //data: [12, 19, 3, 5, 2, 3],
-                data: latencies,
-                // backgroundColor: [
-                //     'rgba(255, 99, 132, 0.2)',
-                //     'rgba(54, 162, 235, 0.2)',
-                //     'rgba(255, 206, 86, 0.2)',
-                //     'rgba(75, 192, 192, 0.2)',
-                //     'rgba(153, 102, 255, 0.2)',
-                //     'rgba(255, 159, 64, 0.2)'
-                // ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        // options: {
-        //     scales: {
-        //         yAxes: [{
-        //             ticks: {
-        //                 beginAtZero: true,
-        //                 callback: (value) => '$' + value
-        //             }
-        //         }]
-        //     }
-        // }
-    };
-    const image = await canvasRenderService.renderToBuffer(configuration);
-    const dataUrl = await canvasRenderService.renderToDataURL(configuration);
-    const stream = canvasRenderService.renderToStream(configuration);
+  try {
+    await perfTest;
+  } catch (perfTestError) {
+    console.log('Error during performance test: ', perfTestError);
+    return;
+  }
 
-    fs.writeFile('testimage.png', image, () => console.log('done'));
+  const configuration = {
+    type: 'line',
+    data: {
+      //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: xAxisLabels,
+      datasets: [{
+        label: 'Latency',
+        //data: [12, 19, 3, 5, 2, 3],
+        data: latencies,
+        // backgroundColor: [
+        //   'rgba(255, 99, 132, 0.2)',
+        //   'rgba(54, 162, 235, 0.2)',
+        //   'rgba(255, 206, 86, 0.2)',
+        //   'rgba(75, 192, 192, 0.2)',
+        //   'rgba(153, 102, 255, 0.2)',
+        //   'rgba(255, 159, 64, 0.2)'
+        // ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    // options: {
+    //     scales: {
+    //         yAxes: [{
+    //             ticks: {
+    //                 beginAtZero: true,
+    //                 callback: (value) => '$' + value
+    //             }
+    //         }]
+    //     }
+    // }
+  };
+  const image = await canvasRenderService.renderToBuffer(configuration);
+  // const dataUrl = await canvasRenderService.renderToDataURL(configuration);
+  // const stream = canvasRenderService.renderToStream(configuration);
+
+  fs.writeFile('testimage.png', image, () => console.log('done'));
 })();
